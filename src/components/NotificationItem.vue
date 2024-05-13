8e4d80e7e64a3af361348e269136a287d589e229
ishu-00 <ishikadahiya22@gmail.com>
2024-05-13 10:33:07 +0530

    Updated code for Dynamically Rendering

--- src/components/NotificationItem.vue
+++ src/components/NotificationItem.vue
@@ -1,271 +1,112 @@
 <template>
-  <div class="divide-y-2 divide-gray-200 text-lg">
-    <!-- Example notification item -->
+  <div class="divide-y-2 divide-gray-400 text-lg px-1">
+    <div v-for="(notification, index) in notifications" :key="index">
 
-    <!-- notification 1 -->
-    <div class="flex items-start justify-between py-5">
-      <!-- User image -->
-      <div class="relative flex-shrink-0">
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGFf3EclldhcVYfE8yFGQKPJqUdYDjl0JJLujbqL053w&s"
-          alt="User"
-          class="w-12 h-12 rounded-full object-cover overflow-hidden"
-        />
-        <!-- Notification type icon -->
-        <span
-          class="material-symbols-outlined absolute bottom-0 right-0 w-5 h-5 rounded-full bg-blue-500 text-white text-xs flex items-center justify-center"
-          style="transform: translate(25%, 0)"
-        >
-          event
-        </span>
-      </div>
-      <div>
-        <!-- Notification content -->
-        <div class="flex flex-col w-full ml-4">
-          <p>
-            <!-- Username -->
-            <span class="text-black font-semibold">Amanda Haydenson</span>
-            <!-- Notification details -->
-            <span class="text-gray-500">
-              changed the delivery date of
-              <span class="text-blue-500 font-semibold">Design project</span>
-              to Sep 25</span
-            >
-          </p>
-          <!-- Time and team name -->
-          <div class="flex items-center justify-start mt-2.5">
-            <span class="text-gray-500 mr-2">Yesterday &bullet;</span>
-            <div class="flex space-x-1">
-              <span
-                class="material-symbols-outlined w-4 h-4 text-lg text-amber-600"
-                >spa</span
-              >
-              <span class="text-gray-500 underline underline-offset-4"
-                >Design Team</span
-              >
-            </div>
-          </div>
+
+      <!-- Main Structure -->
+      <div class="flex items-start justify-between py-5">
+
+
+        <!-- User image -->
+        <div class="relative flex-shrink-0">
+          <img
+            :src="notification.userImage"
+            alt="User"
+            class="w-12 h-12 rounded-full object-cover overflow-hidden"
+          />
+
+
+          <!-- Notification type icon -->
+          <span
+            :class="notificationTypeIconClasses(notification.type)"
+            style="transform: translate(25%, 0)"
+          >
+            {{ notification.type }}
+          </span>
         </div>
-      </div>
-    </div>
+        <div>
 
-    <!-- notification 2 -->
-    <div class="flex items-start justify-between py-5">
-      <!-- User image -->
-      <div class="relative flex-shrink-0">
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXtTKj6DF01bq5aKBesgbJ9QkGL8XafAuFcQ&s"
-          alt="User"
-          class="w-12 h-12 rounded-full object-cover overflow-hidden"
-        />
-        <!-- Notification type icon -->
-        <span
-          class="material-symbols-outlined absolute bottom-0 right-0 w-5 h-5 rounded-full bg-blue-500 text-white text-xs flex items-center justify-center"
-          style="transform: translate(25%, 0)"
-        >
-          notes
-        </span>
-      </div>
-      <div>
-        <!-- Notification content -->
-        <div class="flex flex-col w-full ml-4">
-          <p>
-            <!-- Username -->
-            <span class="text-black font-semibold">Billy the Kid</span>
-            <!-- Notification details -->
-            <span class="text-gray-500">
-              commented on the status of
-              <span class="text-blue-500 font-semibold">Design project</span>
-            </span>
-          </p>
-          <!-- Time and team name -->
-          <div class="flex items-center justify-start mt-2.5">
-            <span class="text-gray-500 mr-2">Yesterday &bullet;</span>
-            <div class="flex space-x-1">
+
+          <!-- Notification content -->
+          <div class="flex flex-col ml-4 flex-grow">
+            <p class="w-full">
+
+
+              <!-- Username -->
+              <span class="text-black font-semibold"
+                >{{ notification.userName }}&nbsp;</span
+              >
+
+
+              <!-- Notification details -->
               <span
-                class="material-symbols-outlined w-4 h-4 text-lg text-amber-600"
-                >spa</span
-              >
-              <span class="text-gray-500 underline underline-offset-4"
-                >Design Team</span
-              >
-            </div>
-          </div>
-          <div class="border-l-4 mt-2.5 px-4">
-            <div>
-              <span class="text-gray-500">
-                <span class="text-black font-semibold">@chunkofunk</span> have
-                you got the latest designs for the new concepts
-              </span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+                class="text-gray-500"
+                v-html="highlightProject(notification.details)"
+              ></span>
+            </p>
 
-    <!-- notification 3 -->
-    <div class="flex items-start justify-between py-5">
-      <!-- User image -->
-      <div class="relative flex-shrink-0">
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0SPOPL_7v8VW82mBqZ5M9PML8Q2v7yq8SWh1tywseGMQCgKO1aHMvF8pZS92RTt51m4w&usqp=CAU"
-          alt="User"
-          class="w-12 h-12 rounded-full object-cover overflow-hidden"
-        />
-        <!-- Notification type icon -->
-        <span
-          class="material-symbols-outlined absolute bottom-0 right-0 w-5 h-5 rounded-full bg-amber-500 text-white text-xs flex items-center justify-center"
-          style="transform: translate(25%, 0)"
-        >
-          notifications
-        </span>
-      </div>
-      <div>
-        <!-- Notification content -->
-        <div class="flex flex-col w-full ml-4">
-          <p>
-            <!-- Username -->
-            <span class="text-black font-semibold">James Hetfield</span>
-            <!-- Notification details -->
-            <span class="text-gray-500">
-              is requesting access to &nbsp;
-              <span class="text-blue-500 font-semibold">Design project</span>
-            </span>
-          </p>
-          <!-- Time and team name -->
-          <div class="flex items-center justify-start mt-2.5">
-            <span class="text-gray-500 mr-2">Yesterday &bullet;</span>
-            <div class="flex space-x-1">
-              <span
-                class="material-symbols-outlined w-4 h-4 text-lg text-amber-600"
-                >spa</span
-              >
-              <span class="text-gray-500 underline underline-offset-4"
-                >Design Team</span
-              >
-            </div>
-          </div>
 
-          <div class="flex items-center justify-start gap-2 mt-2.5">
-            <button
-              class="btn btn-sm rounded-lg bg-white border-2 border-gray-200 shadow-md px-1.5"
-            >
-              Decline
-            </button>
-            <button
-              class="btn btn-sm bg-blue-600 text-white font-thin rounded-lg px-2"
-            >
-              Approve
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- notification 4 -->
-    <div class="flex items-start justify-between py-5">
-      <!-- User image -->
-      <div class="relative flex-shrink-0">
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYcX6p8KrVESV2N1xIl48zHpQoiiI3QipB9wSJHhrl0GMRiTciLUeVwkPK-cmmnzS2cjg&usqp=CAU"
-          alt="User"
-          class="w-12 h-12 rounded-full object-cover overflow-hidden"
-        />
-        <!-- Notification type icon -->
-        <span
-          class="material-symbols-outlined absolute bottom-0 right-0 w-5 h-5 rounded-full bg-gray-500 text-white text-xs flex items-center justify-center"
-          style="transform: translate(25%, 0)"
-        >
-          edit
-        </span>
-      </div>
-      <div>
-        <!-- Notification content -->
-        <div class="flex flex-col w-full ml-4">
-          <p>
-            <!-- Username -->
-            <span class="text-black font-semibold">Lisa Lowis </span>
-            <!-- Notification details -->
-            <span class="text-gray-500"
-              >cadded the following tags to
-              <span class="text-blue-500 font-semibold"
-                >Design project</span
-              ></span
-            >
-          </p>
-          <!-- Time and team name -->
-          <div class="flex items-center justify-start mt-2.5">
-            <span class="text-gray-500 mr-2">Yesterday &bullet;</span>
-            <div class="flex space-x-1">
-              <span
-                class="material-symbols-outlined w-4 h-4 text-lg text-amber-600"
-                >spa</span
+            <!-- Time and team name -->
+            <div class="flex items-center justify-start mt-2.5">
+              <span class="text-gray-500 mr-2"
+                >{{ notification.timestamp }} &bullet;</span
               >
-              <span class="text-gray-500 underline underline-offset-4"
-                >Design Team</span
-              >
+              <div class="flex space-x-1">
+                <span
+                  class="material-symbols-outlined w-4 h-4 text-lg text-amber-600"
+                  style="transform: translate(0%, -13%)"
+                >
+                  spa
+                </span>
+                <span class="text-gray-500 underline underline-offset-4">{{
+                  notification.teamName
+                }}</span>
+              </div>
             </div>
-          </div>
-          <!-- Tags -->
-          <div class="flex items-center justify-start mt-2.5 gap-2">
-            <span
-              class="bg-blue-100 rounded-lg text-blue-800 px-1.5 py-0 text-semibold"
-              >Design</span
-            >
-            <span
-              class="bg-green-100 rounded-lg text-green-800 px-1.5 py-0 text-semibold"
-              >Saas</span
-            >
-            <span
-              class="bg-teal-100 rounded-lg text-teal-800 px-1.5 py-0 text-semibold"
-              >Project</span
-            >
-          </div>
-        </div>
-      </div>
-    </div>
 
-    <!-- notification 5 -->
-    <div class="flex items-start justify-between py-5">
-      <!-- User image -->
-      <div class="relative flex-shrink-0">
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhP58vfGWfJCqtoQZ8VA2q1XzYr7VJZVa0yEBvQZ4UfnlGZHWxuu1VXkeP-VuK9pVJWMY&usqp=CAU"
-          alt="User"
-          class="w-12 h-12 rounded-full object-cover overflow-hidden"
-        />
-        <!-- Notification type icon -->
-        <span
-          class="material-symbols-outlined absolute bottom-0 right-0 w-5 h-5 rounded-full bg-green-600 text-white text-xs flex items-center justify-center"
-          style="transform: translate(25%, 0)"
-        >
-          upload_file
-        </span>
-      </div>
-      <div>
-        <!-- Notification content -->
-        <div class="flex flex-col w-full ml-4">
-          <p>
-            <!-- Username -->
-            <span class="text-black font-semibold">John Carpenter</span>
-            <!-- Notification details -->
-            <span class="text-gray-500">
-              uploaded a file to
-              <span class="text-blue-500 font-semibold">Design project</span>
-            </span>
-          </p>
-          <!-- Time and team name -->
-          <div class="flex items-center justify-start mt-2.5">
-            <span class="text-gray-500 mr-2">Yesterday &bullet;</span>
-            <div class="flex space-x-1">
-              <span
-                class="material-symbols-outlined w-4 h-4 text-lg text-amber-600"
-                >spa</span
-              >
-              <span class="text-gray-500 underline underline-offset-4"
-                >Design Team</span
-              >
-            </div>
+
+
+            <!-- Additional details based on notification type -->
+            <template v-if="notification.type === 'notes'">
+              <div class="border-l-4 border-gray-400 mt-2.5 px-4">
+                <div>
+                  <span class="text-gray-500">
+                    <span class="text-black font-semibold"
+                      >@{{ notification.additionalData.username }}</span
+                    >
+                    {{ notification.additionalData.message }}
+                  </span>
+                </div>
+              </div>
+            </template>
+            <template v-else-if="notification.type === 'notifications'">
+              <div class="flex items-center justify-start gap-2 mt-2.5">
+                <button
+                  class="btn btn-sm rounded-lg bg-white border-2 border-gray-200 px-1.5"
+                >
+                  Decline
+                </button>
+                <button
+                  class="btn btn-sm bg-blue-600 text-white font-thin rounded-lg px-2"
+                >
+                  Approve
+                </button>
+              </div>
+            </template>
+            <template v-else-if="notification.type === 'edit'">
+
+
+              <!-- Tags -->
+              <div class="flex items-center justify-start mt-2.5 gap-2">
+                <span
+                  v-for="(tag, index) in notification.tags"
+                  :key="index"
+                  :class="getTagClasses(tag.color)"
+                >
+                  {{ tag.name }}
+                </span>
+              </div>
+            </template>
           </div>
         </div>
       </div>
@@ -275,6 +116,96 @@
 
 <script>
 export default {
-  name: "NotificationItem",
+  data() {
+    return {
+      notifications: [
+        {
+          userImage:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGFf3EclldhcVYfE8yFGQKPJqUdYDjl0JJLujbqL053w&s",
+          userName: "Amanda Haydenson",
+          type: "event",
+          details: "changed the delivery date of Design project to Sep 25",
+          timestamp: "Yesterday",
+          teamName: "Design Team",
+        },
+        {
+          userImage:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXtTKj6DF01bq5aKBesgbJ9QkGL8XafAuFcQ&s",
+          userName: "Billy the Kid",
+          type: "notes",
+          details: "commented on the status of Design project",
+          timestamp: "Yesterday",
+          teamName: "Design Team",
+          additionalData: {
+            username: "chunkofunk",
+            message: "have you got the latest designs for the new concepts",
+          },
+        },
+        {
+          userImage:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0SPOPL_7v8VW82mBqZ5M9PML8Q2v7yq8SWh1tywseGMQCgKO1aHMvF8pZS92RTt51m4w&usqp=CAU",
+          userName: "James Hetfield",
+          type: "notifications",
+          details: "is requesting access to Design project",
+          timestamp: "Yesterday",
+          teamName: "Design Team",
+        },
+        {
+          userImage:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYcX6p8KrVESV2N1xIl48zHpQoiiI3QipB9wSJHhrl0GMRiTciLUeVwkPK-cmmnzS2cjg&usqp=CAU",
+          userName: "Lisa Lowis",
+          type: "edit",
+          details: "added the following tags to Design project",
+          timestamp: "Yesterday",
+          teamName: "Design Team",
+          tags: [
+            { name: "Design", color: "blue" },
+            { name: "Saas", color: "green" },
+            { name: "Project", color: "teal" },
+          ],
+        },
+        {
+          userImage:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhP58vfGWfJCqtoQZ8VA2q1XzYr7VJZVa0yEBvQZ4UfnlGZHWxuu1VXkeP-VuK9pVJWMY&usqp=CAU",
+          userName: "John Carpenter",
+          type: "upload_file",
+          details: "uploaded a file to Design project.",
+          timestamp: "Yesterday",
+          teamName: "Design Team",
+        },
+      ],
+    };
+  },
+  methods: {
+    notificationTypeIconClasses(type) {
+      return {
+        "material-symbols-outlined": true,
+        absolute: true,
+        "bottom-0": true,
+        "right-0": true,
+        "w-5": true,
+        "h-5": true,
+        "rounded-full": true,
+        "bg-blue-600": type === "event" || type === "notes",
+        "bg-amber-500": type === "notifications",
+        "bg-green-600": type === "upload_file",
+        "bg-gray-500": type === "edit",
+        "text-white": true,
+        "text-xs": true,
+        flex: true,
+        "items-center": true,
+        "justify-center": true,
+      };
+    },
+    highlightProject(details) {
+      return details.replace(
+        /Design project/g,
+        '<span class="text-blue-500 font-semibold">Design project</span>'
+      );
+    },
+    getTagClasses(color) {
+      return `bg-${color}-200 text-${color}-800 rounded-lg px-1.5 py-0 font-semibold`;
+    },
+  },
 };
 </script>
--- src/views/MainLayout.vue
+++ src/views/MainLayout.vue
@@ -1,12 +1,12 @@
 <template>
   <div
-    class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-2xl pt-7 pb-4 px-10 shadow-lg font-roboto bg-white max-w-lg"
+    class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-2xl pt-6 pb-4 px-7 shadow-lg font-opensans bg-white max-w-lg"
   >
   <!-- Main Head -->
-    <div class="flex justify-between items-center mb-4">
-      <h2 class="text-black font-bold text-xl">Activity Example</h2>
+    <div class="flex justify-between items-center mb-4 font-semibold px-1.5">
+      <h2 class="text-black text-xl">Activity Example</h2>
       <div class="flex items-center space-x-4">
-        <button class="bg-white font-semibold text-blue-600">Delete all</button>
+        <button class="bg-white text-blue-600">Delete all</button>
         <span class="material-symbols-outlined text-gray-600 text-2xl">
           more_vert
         </span>
